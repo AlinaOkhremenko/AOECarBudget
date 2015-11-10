d2fbc38dfa9184df9379a5c7c363b7f838a9e0d5
Alina Okhremenko <slykaramba@gmail.com>
2015-11-10 18:11:48 +0100

    initial commit

---
+++ AOECarBudget/AOECarBudget/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AOECarBudget
+//
+//  Created by Alina Okhremenko on 10.11.15.
+//  Copyright © 2015 Alina Okhremenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AOECarBudget/Sources/UI/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  AOECarBudget
+//
+//  Created by Alina Okhremenko on 10.11.15.
+//  Copyright © 2015 Alina Okhremenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
